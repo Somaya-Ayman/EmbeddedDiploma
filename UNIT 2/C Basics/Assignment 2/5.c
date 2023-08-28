c7476c2ebf9efc164e54f93fbe7b907405a66cf3
Somaya Ayman <87386122+Somaya-Ayman@users.noreply.github.com>
2023-08-28 05:41:49 +0300

    Create 5.c

---
+++ UNIT 2/C Basics/Assignment 2/5.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+void main () {
+	char x;
+	printf("Enter a character: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%c", &x);
+	if( ( x >= 'a') && (x<='z') || ( (x>= 'A') && (x<= 'Z')))printf("%c is a charachter", x); 
+	else printf("%c is not a charachter", x);
+}
