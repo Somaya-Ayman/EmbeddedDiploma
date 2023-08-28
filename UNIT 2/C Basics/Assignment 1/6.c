6dcaf7fa3f57cc32f9c3680a4c6a5493e80f83d6
Somaya Ayman <87386122+Somaya-Ayman@users.noreply.github.com>
2023-08-28 05:18:51 +0300

    Create 6.c

---
+++ UNIT 2/C Basics/Assignment 1/6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main () {
+	float a,b;
+	printf("Enter value of a: "); 
+	fflush(stdin); fflush(stdout); 
+	scanf("%f",&a);
+	printf("Enter value of b: "); 
+	fflush(stdin); fflush(stdout); 
+	scanf("%f",&b);
+	float temp = a;
+	a = b;
+	b = temp;
+	printf("After swapping, value of a = %.2f\n", a); 
+	printf("After swapping, value of b = %.2f\n",b);
+}
